248e0d3cef0966c13a0004e96950eb308ab62275
Niranjan Singh Pargai <niranjansinghpargai000@gmail.com>
2026-09-14 11:50:58 +0530

    Add files via upload

---
+++ Data and Problem Statement.sql
@@ -0,0 +1,52 @@
+-- Project: Amazon E-commerce Sale Data Cleaning and Analysis
+-- Data: amazon products sales  [source: Kaggle (thanks)] 
+-- File: 1
+          -- 1. amazon_sale_report
+         
+-- Company: Amazon.com, Inc.
+-- Problem Statement: "How can the business improve sales performance and operational efficiency by 
+--                     identifying high-performing productes, sales trends, customer demand across 
+--                     locations, order cancellations, and fulfilment performance?"
+-- Sales
+-- How many orders were placed?
+-- What is the total revenue?
+-- What is the average quantity per order?
+-- What are the monthly sales trend?
+-- Which dates have the highest/lowest sales?
+-- Which dates have the highest order volume?
+-- Which states generate the most revenue?
+-- Which states have the highest number of orders?
+-- Which states have the highest cancellation rate?
+
+-- Product Analysis
+-- Which categories generate the highest revenue?
+-- Which categories have the highest average selling value?
+-- Which category has the highest cancellation rate?
+-- Which SKUs sell the most?
+-- Which sizes are most frequently ordered?
+-- Which product styles perform best?
+
+-- Order Status
+-- How many orders are shipped?
+-- How many orders are cancelled?
+-- What percentage of orders are cancelled?
+-- How many orders were returned or rejected?
+-- How many orders were lost in transit?
+-- How many orders were damaged?
+
+-- Fulfilment Analysis
+-- Which fulfilment method generates more revenue?
+-- Which fulfilment method has the highest successful delivery rate?
+-- Which fulfiment method has more cancellations?
+-- How does Amazon fulfilment compare with Merchant fulfilment?
+        
+
+
+ 
+
+  
+  
+    
+    
+    
+
